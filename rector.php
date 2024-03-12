c119b00d0a73c3d3a1ab49afce9b941a39b8c062
ToshY <31921460+ToshY@users.noreply.github.com>
2024-03-12 20:27:09 +0100

    simplified rector config

--- rector.php
+++ rector.php
@@ -3,34 +3,27 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(phpVersion: PhpVersion::PHP_81);
     $rectorConfig->importNames();
     $rectorConfig->importShortClasses();
-    $rectorConfig->parallel(seconds: 600, maxNumberOfProcess: 32);
-
-    $rectorConfig->autoloadPaths(autoloadPaths: [
-        __DIR__ . '/vendor/autoload.php',
-    ]);
+    $rectorConfig->parallel();
 
     $rectorConfig->paths(paths: [
         __DIR__ . '/src',
     ]);
 
-    $rectorConfig->skip(criteria: [
-        __DIR__ . '/vendor',
+    $rectorConfig->skip(skip: [
         NullToStrictStringFuncCallArgRector::class => [
             __DIR__ . '/src/TokenAuthentication.php',
         ],
     ]);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
-        LevelSetList::UP_TO_PHP_80,
         LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
+        LevelSetList::UP_TO_PHP_83,
     ]);
 };
