6527e30b33c89f66730221f58a031f578bb1e80a
ToshY <31921460+ToshY@users.noreply.github.com>
2026-09-09 17:04:50 +0000

    Updated API models

--- src/Enum/Validation/Map/Core.php
+++ src/Enum/Validation/Map/Core.php
@@ -50,6 +50,10 @@ use ToshY\BunnyNet\Model\Api\Core\DnsZone\UpdateDnsRecord;
 use ToshY\BunnyNet\Model\Api\Core\DnsZone\UpdateDnsZone;
 use ToshY\BunnyNet\Model\Api\Core\DrmCertificate\ListDrmCertificates;
 use ToshY\BunnyNet\Model\Api\Core\Integration\GetGitHubIntegration;
+use ToshY\BunnyNet\Model\Api\Core\LoadBalancer\GetAttachedPullZones;
+use ToshY\BunnyNet\Model\Api\Core\LoadBalancer\GetLoadBalancerAccountStatistics;
+use ToshY\BunnyNet\Model\Api\Core\LoadBalancer\GetLoadBalancerStatistics;
+use ToshY\BunnyNet\Model\Api\Core\LoadBalancer\GetLoadBalancerUsage;
 use ToshY\BunnyNet\Model\Api\Core\Pricing\GetPriceEstimation;
 use ToshY\BunnyNet\Model\Api\Core\PullZone\AddAllowedReferer;
 use ToshY\BunnyNet\Model\Api\Core\PullZone\AddBlockedIp;
@@ -233,6 +237,10 @@ final class Core
         GetStatistics::class => ModelValidationStrategy::STRICT_QUERY,
         GlobalSearch::class => ModelValidationStrategy::STRICT_QUERY,
         GetPriceEstimation::class => ModelValidationStrategy::STRICT_QUERY,
+        GetLoadBalancerStatistics::class => ModelValidationStrategy::STRICT_QUERY,
+        GetAttachedPullZones::class => ModelValidationStrategy::STRICT_QUERY,
+        GetLoadBalancerAccountStatistics::class => ModelValidationStrategy::STRICT_QUERY,
+        GetLoadBalancerUsage::class => ModelValidationStrategy::STRICT_QUERY,
         GetDnsZoneQueryStatistics::class => ModelValidationStrategy::STRICT_QUERY,
         EnableDnssecOnDnsZone::class => ModelValidationStrategy::NONE,
         DisableDnssecOnDnsZone::class => ModelValidationStrategy::NONE,
